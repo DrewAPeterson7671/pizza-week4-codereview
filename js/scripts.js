43cbd5c90fe54fb9a6986dbd8d95a9e661290abf
Student <example@example.com>
2019-11-08 10:53:03 -0800

    Make all size-based price branching functional and tested.

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,20 @@ function Pizza(customerName, pizzaSize, pizzaToppings) {
 Pizza.prototype.calcPrice = function() {
   if (this.pizzaSize === "extra large") {
     this.pizzaPrice += 14;
-  } else {
-    return console.log("No Joy");
-  }
-  return this.pizzaPrice;
-};
+    } else if (this.pizzaSize === "large") {
+      this.pizzaPrice += 12;
+    } else if (this.pizzaSize === "medium") {
+      this.pizzaPrice += 10;
+    } else if (this.pizzaSize === "small") {
+      this.pizzaPrice += 8;
+    } else {
+      return console.log("No Joy");
+    }
+      return this.pizzaPrice;
+  };
 
 
 
-var pizzaOrder = new Pizza("George", "extra large", ["Pepperoni", "Olives", "Mushrooms"])
+var pizzaOrder = new Pizza("George", "small", ["Pepperoni", "Olives", "Mushrooms"])
 
 console.log(pizzaOrder.calcPrice());
