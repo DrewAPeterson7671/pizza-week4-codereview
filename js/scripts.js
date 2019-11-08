01aa4232a75691b6f2aa4725255f852cb2db2e15
Student <example@example.com>
2019-11-08 10:03:59 -0800

    Start a framework for the initial prototype functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-function Pizza(customerName, pizzaSize, pizzaToppings,pizzaPrice) {
+function Pizza(customerName, pizzaSize, pizzaToppings) {
   this.customerName = customerName,
   this.pizzaSize = pizzaSize,
   this.pizzaToppings = pizzaToppings,
-  this.pizzaPrice = pizzaPrice
+  this.pizzaPrice = 0
 }
 
+Pizza.prototype.calcPrice = function() {
+  if (this.pizzaSize == "extra large") {
+    this.pizzaPrice = 14;
+    return this.pizzaPrice;
+  } else {
+    return console.log("No Joy");
+  }
+};
 
+var pizzaOrder = new Pizza("George", "extra large", ["Pepperoni", "Olives", "Mushrooms"])
 
-var pizzaOrder = new Pizza("George", "medium", ["Pepperoni", "Olives", "Mushrooms"], "0")
-console.log(pizzaOrder);
+test1 = pizzaOrder.calcPrice;
+
+console.log(test1);
